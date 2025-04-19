515c25ce69ec32006ebf06057121d6689fa46743
Shalni Bharti <shalni.bharti@aitoxr.com>
2025-04-19 10:02:17 +0000

    Feedback Description Fix

--- src/components/ui/Feedback.tsx
+++ src/components/ui/Feedback.tsx
@@ -8,7 +8,7 @@ interface ProfileCardProps {
     rating: string;
 }
 
-function ProfileCard({ name, age, sport, description, rating }: ProfileCardProps) {
+function FeedbackSection({ name, age, sport, description, rating }: ProfileCardProps) {
     return (
         <div className="border font-metropolis border-[#BCBCBC] rounded-[15px] py-[10px] px-[13px] h-[166px] w-[316px]">
             <div className="flex justify-between items-center mb-2">
@@ -19,23 +19,9 @@ function ProfileCard({ name, age, sport, description, rating }: ProfileCardProps
                 </div>
             </div>
             <p className="text-[#FFF] text-[12px] mb-2">{`M • ${age} • ${sport}`}</p>
-            <p className="text-white opacity-70 text-[10px] leading-[12px] h-[60px] overflow-hidden">
+            <p className="text-white opacity-70 text-[10px] leading-[12px] overflow-hidden">
                 {description}
             </p>
-        </div>
-    );
-}
-
-function FeedbackSection({ name, age, sport, description, rating }: ProfileCardProps) {
-    return (
-        <div>
-            <ProfileCard
-                name={name}
-                age={age}
-                sport={sport}
-                description={description}
-                rating={rating}
-            />
         </div>
     );
 }
