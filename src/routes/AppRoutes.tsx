1f90164cbd570557b9c677088d374904084f0e54
Shalni Bharti <shalni.bharti@aitoxr.com>
2025-04-28 13:07:30 +0000

    T&C and Privacy Policy page added

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from '../pages/page/Landing';
+import TermsAndConditions from '../pages/page/TermsCondition';
+import PrivacyPolicy from '../pages/page/PrivacyPolicy';
+
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/landingPage" element={<LandingPage />} />
+            <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
     );
 };
